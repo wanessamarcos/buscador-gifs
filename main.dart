3334de084c6bf12ea2365382d955353d1e504518
wanessamarcos <102993709+wanessamarcos@users.noreply.github.com>
2022-10-14 15:20:30 -0300

    Delete main.dart

--- main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:buscador_de_gifs/ui/home_page.dart';
-void main(){
-  runApp(MaterialApp(
-    home: HomePage(),
-    theme: ThemeData(hintColor: Colors.white),
-  ));
-}
